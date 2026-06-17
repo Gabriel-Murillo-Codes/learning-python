ab8170d0eea6475c4755945a6b0292db481821a1
Gabriel Murillo-Marengo <gmurillomare@gmail.com>
2026-06-17 02:00:19 -0500

    Add for loop to itemize transactions

---
+++ lists.py
@@ -0,0 +1,30 @@
+transactions = []
+large = 0
+medium = 0
+small = 0
+entry = input("Enter all of your transactions here. When finished type done. ")
+while entry != ("done"):
+    try:
+        amount = float(entry)
+    except ValueError:
+        print("That wasn't a valid number. Try again. ")
+        entry = input("Enter all of your transactions here. When finished type done. ")
+        continue
+    if amount >= 100:
+        transactions.append(amount)
+        large = large + amount
+    elif amount >= 20:
+        transactions.append(amount)
+        medium = medium + amount
+    elif amount >= 0:
+        transactions.append(amount)
+        small = small + amount
+    else: 
+        print("A transaction can not be a negative number. ")
+    entry = input("Enter all of your transactions here. When finished type done. ")
+print(large)
+print(medium)
+print(small)
+print(transactions)
+for transaction in transactions:
+    print("Transaction: ", transaction)
--- loops.py
+++ loops.py
@@ -1,27 +1,3 @@
-#count = 0
-#while count < 5:
-    #print(count)
-    #count = count + 1
-#amount in [10, 20, 30]:
-    #print(amount)
-
-#for i in range(5):
-    #print(i)
-#--------------------------------------
-#total = 0
-#for amount in [12.50, 8.99, 45.00, 3.25]:
-    #print(amount)
-    #total = total + amount
-#print(total)
-#---------------------------------------
-#total = 0
-#transaction_amount = input("Enter all of your transactions. When finished type done. ")
-#while transaction_amount != ("done"):
-#    total = total + float(transaction_amount)
-#    transaction_amount = input("Enter all of your transactions. When finished type done. ")
-#print(total)
-#----------------------------------------
-#Code breaks with non-numeric input / fix later.
 large = 0
 medium = 0
 small = 0
